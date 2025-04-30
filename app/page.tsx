c1cbe06083ff4ee3090e0f69838c7f906859cde3
Nidheesh <nidheeshdevops@gmail.com>
2025-04-30 08:23:17 +0530

    feat: Update CV download link to open in a new tab

--- app/page.tsx
+++ app/page.tsx
@@ -15,18 +15,24 @@ const Home = () => {
               Hello I&apos;m <br /> <span className="text-accent">Nidheesh</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">
-            I excel at crafting elegant digital experiences and I am proficient in various programming languages and technologies
+              I excel at crafting elegant digital experiences and I am proficient in various programming languages and technologies
             </p>
 
             <div className="flex flex-col xl:flex-row items-center gap-8">
-              <Button variant="border" size="lg" className="uppercase flex items-center gap-2">
-                <span>Download CV</span>
-                <FiDownload className="text-xl" />
-              </Button>
+              <a
+                href="https://docs.google.com/document/d/1THQ9Win_38vEKCmudrBK_h-QkXYGslKKseSVmIQLKpw/edit?usp=drive_link"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Button variant="border" size="lg" className="uppercase flex items-center gap-2">
+                  <span>Download CV</span>
+                  <FiDownload className="text-xl" />
+                </Button>
+              </a>
               <div className="mb-8 xl:mb-0">
-                <Social 
-                containerStyles="flex gap-6"
-                iconStyles="w-9 h-9 border border-accent rounded-full flex
+                <Social
+                  containerStyles="flex gap-6"
+                  iconStyles="w-9 h-9 border border-accent rounded-full flex
                 justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
                 />
               </div>
